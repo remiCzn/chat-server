b05cca156c3b1e1a0ea4e5183a390c7cfccbe78f
remiCzn <cazinremi2@gmail.com>
2021-05-24 23:29:19 +0200

    Add first chat implementation

--- ws/wsServer.js
+++ ws/wsServer.js
@@ -3,18 +3,29 @@ const express = require("express");
 const app = express();
 const server = http.createServer(app);
 const { Server } = require("socket.io");
-const io = new Server(server);
+const io = new Server(server, {
+  cors: {},
+});
+const axios = require("axios");
 
 const config = require("../config");
 
 io.on("connection", (socket) => {
-  console.log("connected");
-  socket.on("new_message", () => {
-    console.log("good!!");
-    socket.emit("message", {});
+  socket.on("new_message", (data) => {
+    axios
+      .post("http://localhost:" + config.ServerPort + "/api/message/post", {
+        jwt: data.token,
+        content: data.content,
+      })
+      .then(() => {
+        io.sockets.emit("message", {});
+      })
+      .catch((err) => {
+        console.log(err);
+      });
   });
 });
 
 server.listen(config.WSServerPort, () => {
-  console.log("WS server listening on port 3000");
+  console.log("WS server listening on port " + config.WSServerPort);
 });
